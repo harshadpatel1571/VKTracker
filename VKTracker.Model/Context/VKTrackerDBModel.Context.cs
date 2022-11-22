fb48962e8a39f22deacd72484c755b3efbde8f78
harshadpatel1571 <harshadpatel1571@gmail.com>
2022-11-22 21:27:00 +0530

    Entity Update for Organization log

--- VKTracker.Model/Context/VKTrackerDBModel.Context.cs
+++ VKTracker.Model/Context/VKTrackerDBModel.Context.cs
@@ -33,5 +33,6 @@ namespace VKTracker.Model.Context
         public virtual DbSet<StockCode> StockCodes { get; set; }
         public virtual DbSet<User> Users { get; set; }
         public virtual DbSet<UserOrganization> UserOrganizations { get; set; }
+        public virtual DbSet<OrganizationLog> OrganizationLogs { get; set; }
     }
 }
