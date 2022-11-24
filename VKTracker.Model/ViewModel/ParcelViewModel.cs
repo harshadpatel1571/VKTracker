9f0d7c56b92ecd7262bc52bd85c17b410ff2eb71
harshadpatel1571 <harshadpatel1571@gmail.com>
2022-11-25 00:07:10 +0530

    Add design for Parcel Report Master for user side

---
+++ VKTracker.Model/ViewModel/ParcelViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VKTracker.Model.ViewModel
+{
+    public class ParcelViewModel : BaseModel
+    {
+        public int Id { get; set; }
+
+        public int ParcelId { get; set; }
+
+        public int LocationId { get; set; }
+
+        public string ChallanNo { get; set; }
+
+        public DateTime DishpatchDate { get; set; }
+
+        public DateTime ArrivalDate { get; set; }
+    }
+}
---
+++ VKTracker/Controllers/ParcelController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VKTracker.Model.ViewModel;
+
+namespace VKTracker.Controllers
+{
+    [Authorize]
+    public class ParcelController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Organization = new SelectList(new List<BindDropdownViewModel>(), "Id", "Name");
+            return View();
+        }
+    }
+}
