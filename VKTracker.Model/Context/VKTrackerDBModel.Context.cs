564be04841fcfb19bd1ba97dc7876bc647383434
harshadpatel1571 <harshadpatel1571@gmail.com>
2022-11-28 22:33:14 +0530

    update customertable entity

--- VKTracker.Model/Context/VKTrackerDBModel.Context.cs
+++ VKTracker.Model/Context/VKTrackerDBModel.Context.cs
@@ -42,5 +42,6 @@ namespace VKTracker.Model.Context
         public virtual DbSet<Userlog> Userlogs { get; set; }
         public virtual DbSet<ParcelReport> ParcelReports { get; set; }
         public virtual DbSet<ParcelReportLog> ParcelReportLogs { get; set; }
+        public virtual DbSet<Customer> Customers { get; set; }
     }
 }
