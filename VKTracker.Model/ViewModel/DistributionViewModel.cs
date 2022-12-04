8d97d67238f004a30c2464307aab9b673d7bb01f
harshadpatel1571 <harshadpatel1571@gmail.com>
2022-12-04 13:24:01 +0530

    design for distribution module

---
+++ VKTracker.Model/ViewModel/DistributionViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VKTracker.Model.ViewModel
+{
+    public class DistributionViewModel
+    {
+        public int StockCodeId { get; set; }
+        public int FabricId { get; set; }
+        public int ItemTypeId { get; set; }
+        public int AvailableQuantity { get; set; }
+        public int LocationId { get; set; }
+        public int StockNo { get; set; }
+        public int PartyId { get; set; }
+        public int Quantity { get; set; }
+        public string BillNo { get; set; }
+        public string Note { get; set; }
+        public DateTime? DistributionDate { get; set; }
+    }
+}
---
+++ VKTracker/Controllers/DistributionController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using VKTracker.Model.ViewModel;
+using VKTracker.Repository.Repository;
+
+namespace VKTracker.Controllers
+{
+    [Authorize]
+    public class DistributionController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            var model = new DistributionViewModel();
+            var repoItem = new ItemRepository();
+            var repoFebric = new FabricRepository();
+            var repoStockCode = new StockCodeRepository();
+            var repoLocation = new LocationRepository();
+
+            ViewBag.StockCodeDDL = new SelectList(await repoStockCode.BindStockCodeDDl(), "Id", "Name");
+            ViewBag.FabricDDL = new SelectList(await repoFebric.BindFabricDDl(), "Id", "Name");
+            ViewBag.ItemTypeDDL = new SelectList(await repoItem.BindItemDDl(), "Id", "Name");
+            ViewBag.LocationDDL = new SelectList(await repoLocation.BindLocationDDl(), "Id", "Name");
+            return View(model);
+        }
+    }
+}
